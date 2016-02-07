7443c6c909a4808bdbecedf2dbc541314c9b429c
Jon Pinhorn <jonpinhorn.typedesign@gmail.com>
2016-02-07 15:32:35 +0000

    070216. Initial upload.
    First upload of all the Atithi project work.

---
+++ build.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+
+import hindkit as kit
+kit.confirm_version('0.3.0')
+
+# - - -
+
+family = kit.Family(
+    trademark = 'Hind Jalandhar',
+    script = 'Gurmukhi',
+    hide_script_name = True,
+)
+
+family.set_masters(
+    modules = [
+        # 'kerning',
+        'mark_positioning',
+        'mark_to_mark_positioning',
+        # 'devanagari_matra_i_variants',
+    ],
+)
+
+family.set_styles()
+
+# - - -
+
+builder = kit.Builder(family)
+
+builder.fontrevision = '1.000'
+
+builder.set_options([
+
+    'prepare_styles',   # stage i
+    'prepare_features', # stage ii
+    'compile',          # stage iii
+
+    'makeinstances', #!
+    'checkoutlines', #!
+    # 'autohint',      #!
+
+    'do_style_linking',
+    'use_os_2_version_4',
+    'prefer_typo_metrics',
+    'is_width_weight_slope_only',
+
+])
+
+builder.import_glyphs()
+
+builder.generate_designspace()
+builder.generate_fmndb()
+
+kit.builder.WriteFeaturesMarkFDK.kIndianAboveMarks = 'top'
+kit.builder.WriteFeaturesMarkFDK.kIndianBelowMarks = 'bottom'
+
+builder.build()
